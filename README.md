e60fdd951aff79c8ac58e571335fd9b4eb5984f9
patty.hao <patty.hao@lrctl.com>
2024-05-29 18:18:56 +0800

    Feat: Add OCR Page & show a image but can not recognize

--- README.md
+++ README.md
@@ -14,3 +14,36 @@
 ## reactnative Navigation
 [https://reactnavigation.org/](https://reactnavigation.org/)
 
+## tree
+
+    myapp:.
+    │   .eslintrc.js
+    │   .gitignore
+    │   .prettierrc.js
+    │   .watchmanconfig
+    │   AndroidManifest.xml
+    │   app.json
+    │   babel.config.js
+    │   Gemfile
+    │   index.js
+    │   jest.config.js
+    │   metro.config.js
+    │   package-lock.json
+    │   package.json
+    │   README.md
+    │   tsconfig.json
+    │   yarn.lock
+    │
+    ├───android
+    ├───ios
+    ├───view
+    │       App.tsx
+    │       IDcard.png
+    │       Index.tsx
+    │       Login.tsx
+    │       OCRPage.tsx
+    │       RFIDPage.tsx
+    │       SettingsView.tsx
+    │
+    └───__tests__
+            App.test.tsx
--- view/OCRPage.tsx
+++ view/OCRPage.tsx
@@ -1,4 +1,5 @@
 //OCRPage.tsx
+/*
 import React from 'react';
 import { View, Text } from 'react-native';
 
@@ -9,5 +10,54 @@ const OCRPage = () => {
     </View>
   );
 };
+
+export default OCRPage;*/
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import TesseractOcr, { LANG_ENGLISH } from 'react-native-tesseract-ocr';
+
+const OCRPage = () => {
+  const [recognizedText, setRecognizedText] = useState('');
+  const imageSource = require('../assets/IDcard.png'); // Updated image source path
+
+  useEffect(() => {
+    performOCR();
+  }, []);
+
+  const performOCR = async () => {
+    try {
+      const tessOptions = {};
+
+      const recognized = await TesseractOcr.recognize(imageSource, LANG_ENGLISH, tessOptions);
+      setRecognizedText(recognized);
+    } catch (error) {
+      console.warn('OCR Error:', error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={imageSource} style={styles.image} />
+      <Text style={styles.text}>{recognizedText || 'OCR TEXT'}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default OCRPage;
