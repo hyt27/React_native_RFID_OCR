7be12d4d14196370feb0a1d45874d415c51ac20e
patty.hao <patty.hao@lrctl.com>
2024-06-04 14:50:28 +0800

    Feat: implement OCR Function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.tess.two)
+    implementation(libs.play.services.vision)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/andoid_ocr/MainActivity.java
+++ app/src/main/java/com/example/andoid_ocr/MainActivity.java
@@ -1,164 +1,186 @@
+//MainActivity.java
 package com.example.andoid_ocr;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.googlecode.tesseract.android.TessBaseAPI;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PERMISSION_REQUEST_CAMERA = 2;
+    private static final String ID_PATTERN = ".*CWR.*";
 
-    public static final String TESS_DATA = "/tessdata";
-    private static final String TARGET_FILENAME = "IDcard.png";
-    private static final String DATA_FILENAME = "eng.traineddata";
-    private static final String TAG = MainActivity.class.getSimpleName();
-
-    private Button main_bt_recognize;
-    private TextView main_tv_result;
-    private ImageView main_iv_image;
+    private TextView textView, textView2;
+    private EditText editText;
+    private ImageView imageView;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 设置布局文件
         setContentView(R.layout.activity_main);
-        // 检查并请求应用所需权限
-        checkPermission();
-        // 获取控件对象
-        initView();
-        // 设置控件的监听器
-        setListener();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                PackageManager.PERMISSION_GRANTED);
+
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+        imageView = findViewById(R.id.imageView);
     }
 
-    private void setListener() {
-        // 设置识别按钮的监听器
-        main_bt_recognize.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 识别之前需要再次检查一遍权限
-                checkPermission();
-                // 点击后的主程序
-                mainProgram();
+    public void buttonCapturePhoto(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        } else {
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
-        });
-    }
-
-    // 获得界面需要交互的控件
-    private void initView() {
-        main_bt_recognize = findViewById(R.id.main_bt_recognize);
-        main_tv_result = findViewById(R.id.main_tv_result);
-        main_iv_image = findViewById(R.id.main_iv_image);
-    }
-
-    // 检查应用所需的权限，如不满足则发出权限请求
-    private void checkPermission() {
-        if (ContextCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 120);
-        }
-        if (ContextCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 121);
         }
     }
 
-    // OCR识别的主程序
-    private void mainProgram() {
-        // 从assets中获取一张Bitmap图片
-        Bitmap bitmap = getBitmapFromAssets(MainActivity.this, TARGET_FILENAME);
-        // 同时显示在界面
-        main_iv_image.setImageBitmap(bitmap);
-        if (bitmap != null) {
-            // 准备工作：创建路径和Tesserect的数据
-            prepareTess();
-            // 初始化Tesserect
-            TessBaseAPI tessBaseAPI = new TessBaseAPI();
-            String dataPath = getExternalFilesDir("/").getPath() + "/";
-            tessBaseAPI.init(dataPath, "eng");
-            // 识别并显示结果
-            String result = getOCRResult(tessBaseAPI, bitmap);
-            main_tv_result.setText(result);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            imageView.setImageBitmap(imageBitmap);
+            performOCR(imageBitmap);
         }
     }
 
-    // 从assets中读取一张Bitmap类型的图片
-    private Bitmap getBitmapFromAssets(Context context, String filename) {
-        Bitmap bitmap = null;
-        AssetManager assetManager = context.getAssets();
+    private void performOCR(Bitmap imageBitmap) {
         try {
-            InputStream is = assetManager.open(filename);
-            bitmap = BitmapFactory.decodeStream(is);
-            is.close();
-            Log.i(TAG, "图片读取成功。");
-            Toast.makeText(getApplicationContext(), "图片读取成功。", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            Log.i(TAG, "图片读取失败。");
-            Toast.makeText(getApplicationContext(), "图片读取失败。", Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        }
-        return bitmap;
-    }
+            TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+            Frame frameImage = new Frame.Builder().setBitmap(imageBitmap).build();
+            SparseArray<TextBlock> textBlockSparseArray = textRecognizer.detect(frameImage);
+            String stringImageText = "";
+            for (int i = 0; i < textBlockSparseArray.size(); i++) {
+                TextBlock textBlock = textBlockSparseArray.get(textBlockSparseArray.keyAt(i));
+                stringImageText = stringImageText + " " + textBlock.getValue();
+            }
+            //textView.setText(stringImageText);
+            System.out.println("whole string: " + stringImageText);
+            String[] lines = stringImageText.split("\n");
+            //textView2.setText(lines[1]);
 
-    // 为Tesserect复制(从assets中复制过去)所需的数据
-    private void prepareTess() {
-        try{
-            // 先创建必须的目录
-            File dir = getExternalFilesDir(TESS_DATA);
-            if(!dir.exists()){
-                if (!dir.mkdir()) {
-                    Toast.makeText(getApplicationContext(), "目录" + dir.getPath() + "没有创建成功", Toast.LENGTH_SHORT).show();
+            String extractedID = "Not Found";
+            String extractedName = "";
+
+            for (String line : lines) {
+                System.out.println("line:" + line);
+                if (matchesPattern(line, ID_PATTERN)) {
+                    System.out.println("match:" + line);
+                    //textView2.setText(line);
+                    int startIndex = line.indexOf("CWR");
+                    extractedID = line.substring(startIndex);
+                } else if (!line.isEmpty()) {
+                    //extractedName = line;
+                    //extractedID = "Not found";
+
                 }
             }
-            // 从assets中复制必须的数据
-            String pathToDataFile = dir + "/" + DATA_FILENAME;
-            if (!(new File(pathToDataFile)).exists()) {
-                InputStream in = getAssets().open(DATA_FILENAME);
-                OutputStream out = new FileOutputStream(pathToDataFile);
-                byte[] buff = new byte[1024];
-                int len;
-                while ((len = in.read(buff)) > 0) {
-                    out.write(buff, 0, len);
-                }
-                in.close();
-                out.close();
-            }
+            String[] filteredLines = filterLines(lines);
+            extractedName = extractName(filteredLines);
+
+            String resultText = "ID: " + extractedID + "\nName: " + extractedName;
+            textView2.setText(resultText);
+
         } catch (Exception e) {
-            Log.e(TAG, e.getMessage());
+            textView2.setText("Failed");
         }
     }
 
-    // 进行OCR并返回识别结果
-    private String getOCRResult(TessBaseAPI tessBaseAPI, Bitmap bitmap) {
-        tessBaseAPI.setImage(bitmap);
-        String result = "-";
-        try{
-            result = tessBaseAPI.getUTF8Text();
-        }catch (Exception e){
-            Log.e(TAG, e.getMessage());
+    private boolean matchesPattern(String line, String pattern) {
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(line);
+        return matcher.matches();
+    }
+
+    private String[] filterLines(String[] lines) {
+        List<String> filteredLinesList = new ArrayList<>();
+        for (String line : lines) {
+            if (!line.isEmpty() && !isIrrelevant(line)) {
+                filteredLinesList.add(line);
+            }
         }
-        tessBaseAPI.end();
-        return result;
+        return filteredLinesList.toArray(new String[0]);
+
+    }
+
+    private boolean isIrrelevant(String line) {
+        return line.contains("construction") ||
+                line.contains("industry") ||
+                line.contains("date") ||
+                line.matches(".*\\d{2}-\\d{2}-\\d{4}.*") ||
+                line.contains("card") ||
+                line.contains("use") ||
+                line.contains("dsba") ||
+                line.contains("no") ||
+                line.contains("oree") ||
+                line.contains("eotta") ||
+                line.contains("osblxi") ||
+                line.contains("datie") ||
+                line.contains("struction") ||
+                line.contains("dale") ||
+                line.contains("canst") ||
+                line.contains("criminaloitence") ||
+                line.contains("offence") ||
+                line.contains("feetabe") ||
+                line.contains("daie") ||
+                line.contains("mmmm") ||
+                line.contains("lree") ||
+                line.contains("ebe") ||
+                line.contains("i\\") ||
+                line.contains("registration") ||
+                Arrays.stream(line.split(" ")).anyMatch(word -> word.length() == 1) ||
+                line.contains("eref") ||
+                line.contains("adis") ||
+                line.matches(".*\\d.*");
+    }
+
+    private String extractName(String[] lines) {
+        for (String line : lines) {
+            // Extract the name from lines containing at least one space
+            if (line.contains(" ")) {
+                return line;
+            }
+        }
+        return "Not Found";
     }
 }
